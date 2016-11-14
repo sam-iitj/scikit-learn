5d0dceaa1dd94da5d5cda066af4f17ecb7156ffb
Satyam Saxena <satyam@freshdesk.com>
2016-11-14 14:44:34 +0530

    Added update print

--- sklearn/linear_model/passive_aggressive.py
+++ sklearn/linear_model/passive_aggressive.py
@@ -263,12 +263,12 @@ class PassiveAggressiveRegressor(BaseSGDRegressor):
             Error computed for each training sample
         """
         if X.ndim == 1:
-            update = ((np.sign(e[0]) * max(abs(e[0]) - self.epsilon, 0))/(np.linalg.norm(X, 2) + (1.0/(2 * self.C)))) 
+            update = ((np.sign(e[0]) * max(abs(e[0]) - self.epsilon, 0))/(np.linalg.norm(X, 2) + (1.0/(2.0 * self.C)))) 
             self.coef_ += X * update
             self.intercept_ += update
         elif X.ndim == 2:
             for i in range(X.shape[0]):
-                update = ((np.sign(e[i]) * max(abs(e[i]) - self.epsilon, 0))/(np.linalg.norm(X[i, :],2) + (1.0/(2 * self.C))))
+                update = ((np.sign(e[i]) * max(abs(e[i]) - self.epsilon, 0))/(np.linalg.norm(X[i, :],2) + (1.0/(2.0 * self.C))))
                 self.coef_ += X[i, :] * update
                 self.intercept_ += update
 
